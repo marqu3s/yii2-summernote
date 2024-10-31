d997acb5f1ca251d42af150f1ac96636ca0ef4ce
João Marques <joao@jjmf.com>
2024-10-31 10:59:52 -0300

    Update CodemirrorAsset.php

--- CodemirrorAsset.php
+++ CodemirrorAsset.php
@@ -7,17 +7,17 @@ use yii\web\AssetBundle;
 class CodemirrorAsset extends AssetBundle
 {
     /** @var string */
-    public $sourcePath = '@bower/codemirror';
+    public $baseUrl = 'https://cdn.jsdelivr.net/npm/codemirror@5.65.18/';
 
     /**
      * @inheritdoc
      */
     public function init()
     {
-        $this->css[] = 'lib/codemirror.css';
+        $this->css[] = 'lib/codemirror.min.css';
         $this->css[] = 'theme/monokai.css';
 
-        $this->js[] = 'lib/codemirror.js';
+        $this->js[] = 'lib/codemirror.min.js';
         $this->js[] = 'mode/xml/xml.js';
 
         parent::init();
