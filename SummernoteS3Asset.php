4bfecab08df58f1802b4c8dbf1134e385e9df860
João Marques <joao@jjmf.com>
2024-10-31 11:27:07 -0300

    Update SummernoteS3Asset.php

--- SummernoteS3Asset.php
+++ SummernoteS3Asset.php
@@ -2,15 +2,12 @@
 
 namespace marqu3s\summernote;
 
-use Yii;
 use yii\web\AssetBundle;
 
 class SummernoteS3Asset extends AssetBundle
 {
     /** @var string */
     public $sourcePath = '@vendor/marqu3s/yii2-summernote/assets';
-
-
 
     /**
      * @inheritdoc
